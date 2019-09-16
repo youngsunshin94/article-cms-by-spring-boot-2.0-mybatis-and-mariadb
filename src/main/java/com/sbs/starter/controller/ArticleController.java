60c3e626fbc70a73ecdc5f11aaf258c4233bb75d
youngsunshin94 <shinysys94@naver.com>
2019-09-16 22:36:05 +0900

    게시물 수정뷰 작업

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -33,6 +33,15 @@ public class ArticleController {
 		
 		return "article/detail";
 	}
+
+	@RequestMapping("/article/modify")
+	public String showModify(Model model, long id) {
+		Article article = articleService.getOne(id);
+		
+		model.addAttribute("article", article);
+		
+		return "article/modify";
+	}
 	
 	@RequestMapping("/article/list")
 	public String showList(Model model) {
