c044c53390ff52dc3661276697f473940686c7ef
youngsunshin94 <shinysys94@naver.com>
2019-09-17 01:27:02 +0900

    MemberService, MemberDao 추가, 회원로그인 ID 중복 처리 기능 추가 후 회원가입에 적용

--- src/main/java/com/sbs/starter/controller/MemberController.java
+++ src/main/java/com/sbs/starter/controller/MemberController.java
@@ -1,17 +1,14 @@
 package com.sbs.starter.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sbs.starter.dto.Article;
-import com.sbs.starter.service.ArticleService;
+import com.sbs.starter.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,8 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j // 이걸 해야 log.info(..) 가 가능, 디버깅 용도
 public class MemberController {
+	@Autowired
+	MemberService memberService;
+	
 	@RequestMapping("/member/join")
 	public String showJoin() {
 			return "member/join";
+	}
+	
+	@RequestMapping("/member/doJoin")
+	@ResponseBody
+	public Map<String , Object> doJoin(@RequestParam Map<String , Object> param ) {
+		// 로그인 ID의 중복성 체크	
+		Map<String,Object> checkLoginIdDupRs =  memberService.checkLoginIdDup((String)param.get("loginId"));
+		
+		return checkLoginIdDupRs;
 	}
 }
---
+++ src/main/java/com/sbs/starter/dao/MemberDao.java
@@ -0,0 +1,10 @@
+package com.sbs.starter.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberDao {
+
+	public int getLoginIdDupCount(String loginId);
+
+}
