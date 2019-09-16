445c53a1a67a7dbc96b7eabf29a9adaedaf86908
youngsunshin94 <shinysys94@naver.com>
2019-09-17 00:52:09 +0900

    회원가입 폼 구현

---
+++ src/main/java/com/sbs/starter/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.sbs.starter.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.starter.dto.Article;
+import com.sbs.starter.service.ArticleService;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Controller
+@Slf4j // 이걸 해야 log.info(..) 가 가능, 디버깅 용도
+public class MemberController {
+	@RequestMapping("/member/join")
+	public String showJoin() {
+			return "member/join";
+	}
+}
